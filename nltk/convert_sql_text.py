8ba185e7a59613641463eb97ad6ff9a8339ff23f
Ying Zhe Chong <zh3zh3@gmail.com>
2012-02-08 20:10:51 -0500

    Updated script to remove unwanted words

--- nltk/convert_sql_text.py
+++ nltk/convert_sql_text.py
@@ -1,13 +1,24 @@
 import pymysql
 import os
+import re
+import nltk
 
-text_path = '/Users/tmlee/TMWorkspace/SeniorDesignDeals/nltk/datatext2'
+text_path = '/home/yingzhe/Desktop/seniordesign/datatext'
+
+def processTitle(title):
+	pattern = re.compile('\w+')
+	postext = nltk.pos_tag(nltk.word_tokenize(title))
+	wordslist = []
+	for w, t in postext:
+		if pattern.match(w) is not None and (t == 'NN' or t == 'NNS' or t == 'NNP' or t == 'NNPS'):
+			wordslist.append(w)
+	return ' '.join(wordslist)
 
 if __name__ == "__main__":
 	categories_names = []
-	conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='mysql')
+	conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='password', db='mysql')
 	cur = conn.cursor()
-	cur.execute("USE deal_db")
+	cur.execute("USE seniordesign")
 	cur.execute("SELECT category_custom FROM dealery GROUP BY category_custom")
 
 	for r in cur:
@@ -21,9 +32,15 @@ if __name__ == "__main__":
 
 		file_count = 0
 		cur.execute("SELECT title FROM dealery WHERE category_custom = '" + c + "'")
+
+		#Create directories for categories if they don't exist
+		if not os.path.exists(text_path + '/' + cat_name):
+			os.makedirs(text_path + '/' + cat_name)
+
 		for t in cur:
-			deal_file = open(text_path + '/' + cat_name + '-' + str(file_count) + '.txt', 'a')
-			deal_file.write(t[0] + '\n')
+			deal_file = open(text_path + '/' + cat_name + '/' + str(file_count) + '.txt', 'w')
+			title = processTitle(t[0])
+			deal_file.write(title + '\n')
 			file_count = file_count + 1
 
 		cur.close()
