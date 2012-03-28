70c58fa0dbfdf8501a01de9faa0e025ea054acee
TM Lee <tmlee@TM-Lees-MacBook-Pro.local>
2012-03-28 01:34:13 -0400

    Rails FrontEnd project with initial fancy box for asking city

---
+++ dealchan_frontend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DealchanFrontend::Application.config.secret_token = 'e5fbca62fda6c7e6a03eedd8f29ee18a7467f839e5b39144a7d9351ac0420aecf8a314c5926e36f03e4cb4aabbfce3baa8a1978e8df67cfa6e23f96c04de2420'
