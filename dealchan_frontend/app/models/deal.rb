077961774372fe1d724232f221c9e545aff35cd5
TM Lee <tmlee@pal-nat184-044-220.itap.purdue.edu>
2012-04-10 17:22:54 -0400

    pulling Deal by category, city name, and page number

--- dealchan_frontend/app/models/deal.rb
+++ dealchan_frontend/app/models/deal.rb
@@ -1,2 +1,15 @@
 class Deal < ActiveRecord::Base
+
+	DEALS_PER_PAGE = 5
+
+
+	def self.get_deal_with_category_and_city(category, city, page_number)
+		where(:category => category, 
+			:city => city)
+		.order("discount DESC")
+		.limit(DEALS_PER_PAGE)
+		.offset(DEALS_PER_PAGE * (page_number-1))
+	end
+
+
 end
