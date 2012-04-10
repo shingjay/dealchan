de9edc625bfd97b62956ad23fa75dbf83303ad9d
TM Lee <tmlee@pal-nat184-044-220.itap.purdue.edu>
2012-04-10 17:29:22 -0400

    get Deal with price range and city with page number sorted by currentPrice

--- dealchan_frontend/app/models/deal.rb
+++ dealchan_frontend/app/models/deal.rb
@@ -2,6 +2,17 @@ class Deal < ActiveRecord::Base
 
 	DEALS_PER_PAGE = 5
 
+	def self.get_deal_with_price_range_and_city(min_price, max_price, city, page_number)
+		where("currentPrice >= :min_price AND currentPrice < :max_price", {
+			:min_price => min_price,
+			:max_price => max_price
+		})
+		.where( :city => city)
+		.order("currentPrice ASC")
+		.limit(DEALS_PER_PAGE)
+		.offset(DEALS_PER_PAGE * (page_number-1))
+	end
+
 
 	def self.get_deal_with_category_and_city(category, city, page_number)
 		where(:category => category, 
