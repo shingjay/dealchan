f5c5639e022a3c6f8667f890ab9ef454d6fef8b7
TM Lee <tmlee@TM-Lees-MacBook-Pro.local>
2012-04-11 16:19:37 -0400

    Deals query by 5 for each slot

--- dealchan_frontend/app/models/deal.rb
+++ dealchan_frontend/app/models/deal.rb
@@ -1,6 +1,24 @@
 class Deal < ActiveRecord::Base
 
 	DEALS_PER_PAGE = 5
+	DEALS_PER_HIGHLIGHT = 5
+
+	def self.get_five_deals_of(args={})
+
+		data = []
+
+		if args[:min_price] && args[:max_price] 
+			data = self.where("currentPrice >= :min_price AND currentPrice < :max_price", {
+				:min_price => args[:min_price],
+				:max_price => args[:max_price]
+			})
+		elsif args[:category]
+			data = self.where(:category => args[:category])
+		end
+		data.where( :city => args[:city])
+		.limit(DEALS_PER_HIGHLIGHT)
+
+	end
 
 	def self.get_deal_with_price_range_and_city(min_price, max_price, city, page_number)
 		where("currentPrice >= :min_price AND currentPrice < :max_price", {
